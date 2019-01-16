384c33a965f79fd9e170ced3759e467ebd407222
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-16 12:59:21 -0700

    Reset initial state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -21,7 +21,7 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload, error: '', loading: false };
+      return { ...state, ...INITIAL_STATE, user: action.payload };
     case LOGIN_USER_FAILED:
       return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     case LOGIN_USER:

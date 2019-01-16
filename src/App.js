fe9fe513162268eec5fe490944c6bf4ef3bcc5aa
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-16 12:35:41 -0700

    Wire up redux and start login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   state = {};
@@ -20,10 +22,12 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View>
-          <Text>Hello!</Text>
+          <LoginForm />
         </View>
       </Provider>
     );

d4960aefe09d47fb3a0421a210c462d778ca9a48
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-16 13:18:58 -0700

    Add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
   state = {};
@@ -26,9 +25,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <View>
-          <LoginForm />
-        </View>
+        <Router />
       </Provider>
     );
   }

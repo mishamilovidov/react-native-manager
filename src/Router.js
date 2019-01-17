8394f5d1228cc970220db8a237421bd737fd63ac
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-16 17:59:10 -0700

    Create add and edit forms

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -11,7 +13,24 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Employee List" />
+          <Scene
+            rightTitle="Add"
+            onRight={() => { Actions.employeeCreate(); }}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employee List"
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+          <Scene
+            key="employeeEdit"
+            component={EmployeeEdit}
+            title="Edit Employee"
+          />
         </Scene>
       </Scene>
     </Router>
